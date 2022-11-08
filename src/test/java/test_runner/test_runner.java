ed1537f2be97a95bbe80e494c0abbde022d4b150
alok14390 <alokkumarpatade@gmail.com>
2022-11-08 19:06:42 +0530

    cucumbercasestudy_TestNG

---
+++ src/test/java/test_runner/test_runner.java
@@ -0,0 +1,14 @@
+package test_runner;
+
+
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+
+@CucumberOptions(features= {"src/test/resources/login/login.feature"},glue= {"definition"},tags="@PositiveTesting",plugin={"html:testoutput/alok.html"})
+public class test_runner extends AbstractTestNGCucumberTests{
+
+}
